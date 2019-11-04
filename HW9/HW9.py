11a67659d2f66649f1200c4d5d3326df7340fbdc
Mosessefah <57191661+Mosessefah@users.noreply.github.com>
2019-11-04 13:56:59 -0500

    Update HW9.py
    Debug and comment for homework 9

--- HW9/HW9.py
+++ HW9/HW9.py
@@ -1,15 +1,16 @@
-import os
+import os  # importing os module
 
 def process_dir(p, indent):
-    for item in os.listdir(path):
-        if os.path.isfile("{0}/{1}".format(path, item)):
-            print("\t"*indent, item)
-        elif os.path.isdir("{0}/{1}".format(path, item)):
-            print("\t"*indent, "\x1b[1;31m{}\x1b[0;0m".format(item))
-            process_dir("{0}/{1}".format(path, item), indent+1)
-        else:
-            print("Not a file or directory")
+	try:
+	    for item in os.listdir(p):
+	        if os.path.isfile("{0}/{1}".format(p, item)):  # to show the files in directory
+	            print("\t"*indent, item)
+	        elif os.path.isdir("{0}/{1}".format(p, item)):  # to show all folders in directory
+	            print("\t"*indent, "\x1b[1;31m{}\x1b[0;0m".format(item))  # to display the directory in red format
+	            # process_dir("{0}/{1}".format(path, item), indent+1)
+	except Exception:
+		print("Not a file or directory.")
 
-path=input("Enter a valid system path: ")
-print("\x1b[1;31m{}\x1b[0;0m".format(path))
-process_dir(path, 1)
+path = input("Enter a valid system path:")
+print("\x1b[1;31m{}\x1b[0;0m".format(path))  # display user input in red format
+process_dir(path, 1)  # importing os module
